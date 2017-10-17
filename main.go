db296c0dbbc6f6ea21bb87f17c8466c7014dd4a6
meng-tint-chien <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-17 20:42:38 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.replyImagemap("http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_1.jpg").Do(); err != nil {
+				if _, err = bot.replyImagemap.baseUrl("http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_1.jpg").Do(); err != nil {
 					log.Print(err)
 				}
 			}
