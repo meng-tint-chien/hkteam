b652b053aa597da6a704aed613f938415c14d296
meng-tint-chien <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-17 19:16:01 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -34,24 +34,69 @@ func main() {
 }
 
 
-func callbackHandler(w http.ResponseWriter, r *http.Request) {
-	events, err := bot.ParseRequest(r)
+var linebot = require('linebot');
+var express = require('express');
+var getJSON = require('get-json');
 
-	var request = require("request");
-  var fs = require("fs");
-  var cheerio = require("cheerio");
-  request({
-    url: "http://blog.infographics.tw",
-    method: "GET"
-  }, function(e,r,b) {
-    if(e || !b) { return; }
-    var $ = cheerio.load(b);
-    var result = [];
-    var titles = $("li.item h2");
-    for(var i=0;i<titles.length;i++) {
-      result.push($(titles[i]).text());
+var bot = linebot({
+  channelId: 'channelId',
+  channelSecret: 'channelSecret',
+  channelAccessToken: 'channelAccessToken'
+});
+
+var timer;
+var pm = [];
+_getJSON();
+
+_bot();
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+
+function _bot() {
+  bot.on('message', function(event) {
+    if (event.message.type == 'text') {
+      var msg = event.message.text;
+      var replyMsg = '';
+      if (msg.indexOf('PM2.5') != -1) {
+        pm.forEach(function(e, i) {
+          if (msg.indexOf(e[0]) != -1) {
+            replyMsg = e[0] + '的 PM2.5 數值為 ' + e[1];
+          }
+        });
+        if (replyMsg == '') {
+          replyMsg = '請輸入正確的地點';
+        }
+      }
+      if (replyMsg == '') {
+        replyMsg = '不知道「'+msg+'」是什麼意思 :p';
+      }
+
+      event.reply(replyMsg).then(function(data) {
+        console.log(replyMsg);
+      }).catch(function(error) {
+        console.log('error');
+      });
     }
-    fs.writeFileSync("result.json", JSON.stringify(result));
   });
+
 }
 
+function _getJSON() {
+  clearTimeout(timer);
+  getJSON('http://opendata2.epa.gov.tw/AQX.json', function(error, response) {
+    response.forEach(function(e, i) {
+      pm[i] = [];
+      pm[i][0] = e.SiteName;
+      pm[i][1] = e['PM2.5'] * 1;
+      pm[i][2] = e.PM10 * 1;
+    });
+  });
+  timer = setInterval(_getJSON, 1800000); //每半小時抓取一次新資料
+}
