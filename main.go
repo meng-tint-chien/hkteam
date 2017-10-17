781dc39e2fb8633ce4c52365554bae3ca8dbee06
meng-tint-chien <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-17 21:27:00 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
@@ -12,45 +16,50 @@ import (
 var bot *linebot.Client
 
 func main() {
-	var err error
-	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
-	log.Println("Bot:", bot, " err:", err)
-	http.HandleFunc("/callback", callbackHandler)
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
-	http.ListenAndServe(addr, nil)
-}
-
-func callbackHandler(message *linebot.TextMessage, w http.ResponseWriter, r *http.Request) {
-	events, err := bot.ParseRequest(r)
-
+	app, err := NewKitchenSink(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+		os.Getenv("APP_BASE_URL"),
+	)
 	if err != nil {
-		if err == linebot.ErrInvalidSignature {
-			w.WriteHeader(400)
-		} else {
-			w.WriteHeader(500)
-		}
-		return
+		log.Fatal(err)
 	}
 
-	for _, event := range events {
-		if event.Type == linebot.EventTypeMessage {
-			switch message.text{
-			case "buttons":
-			imageURL :=  "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_1.jpg"
-			template := linebot.NewButtonsTemplate(
+	// serve /static/** files
+	staticFileServer := http.FileServer(http.Dir("static"))
+	http.HandleFunc("/static/", http.StripPrefix("/static/", staticFileServer).ServeHTTP)
+	// serve /downloaded/** files
+	downloadedFileServer := http.FileServer(http.Dir(app.downloadDir))
+	http.HandleFunc("/downloaded/", http.StripPrefix("/downloaded/", downloadedFileServer).ServeHTTP)
+
+	http.HandleFunc("/callback", app.Callback)
+	// This is just a sample code.
+	// For actually use, you must support HTTPS by using `ListenAndServeTLS`, reverse proxy or etc.
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (app *KitchenSink) handleText(message *linebot.TextMessage, replyToken string, source *linebot.EventSource) error {
+	switch message.Text {
+	
+	case "buttons":
+		imageURL := "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_1.jpg"
+		template := linebot.NewButtonsTemplate(
 			imageURL, "My button sample", "Hello, my button",
 			linebot.NewURITemplateAction("Go to line.me", "https://line.me"),
 			linebot.NewPostbackTemplateAction("Say hello1", "hello ?????", ""),
 			linebot.NewPostbackTemplateAction("ие hello2", "hello ?????", "hello ?????"),
 			linebot.NewMessageTemplateAction("Say message", "Rice=ж╠"),
-			)
-			
-			
-			
-			}
+		)
+		if _, err := app.bot.ReplyMessage(
+			replyToken,
+			linebot.NewTemplateMessage("Buttons alt text", template),
+		).Do(); err != nil {
+			return err
 		}
-	}
+
 }
+
 
 
