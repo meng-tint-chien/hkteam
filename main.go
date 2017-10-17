d39e8c3d4d9492ede3e0de81c53921e1f8558a3e
meng-tint-chien <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-17 20:51:37 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -24,9 +24,30 @@ func main() {
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	events, err := bot.ParseRequest(r)
 
-	client.replyImage(
-  REPLY_TOKEN,
-  'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_1.jpg',
-  'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_1.jpg'
-);
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
+					log.Print(err)
+                                 	bot.replyImage(
+  					REPLY_TOKEN,
+  					'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_1.jpg',
+  					'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_1.jpg'
+					);
+				}
+			}
+		}
+	}
 }
+
+
