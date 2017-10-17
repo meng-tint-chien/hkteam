4c96212b134fca9af006bf83d923300d0778a934
meng-tint-chien <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-17 21:54:17 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -2,80 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
-	"path/filepath"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
+var bot *linebot.Client
+
 func main() {
-	app, err := NewKitchenSink(
-		os.Getenv("CHANNEL_SECRET"),
-		os.Getenv("CHANNEL_TOKEN"),
-		os.Getenv("APP_BASE_URL"),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// serve /static/** files
-	staticFileServer := http.FileServer(http.Dir("static"))
-	http.HandleFunc("/static/", http.StripPrefix("/static/", staticFileServer).ServeHTTP)
-	// serve /downloaded/** files
-	downloadedFileServer := http.FileServer(http.Dir(app.downloadDir))
-	http.HandleFunc("/downloaded/", http.StripPrefix("/downloaded/", downloadedFileServer).ServeHTTP)
-
-	http.HandleFunc("/callback", app.Callback)
-	// This is just a sample code.
-	// For actually use, you must support HTTPS by using `ListenAndServeTLS`, reverse proxy or etc.
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-		log.Fatal(err)
-	}
+	var err error
+	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
+	log.Println("Bot:", bot, " err:", err)
+	http.HandleFunc("/callback", callbackHandler)
+	port := os.Getenv("PORT")
+	addr := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(addr, nil)
 }
 
-// KitchenSink app
-type KitchenSink struct {
-	bot         *linebot.Client
-	appBaseURL  string
-	downloadDir string
-}
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	events, err := bot.ParseRequest(r)
 
-// NewKitchenSink function
-func NewKitchenSink(channelSecret, channelToken, appBaseURL string) (*KitchenSink, error) {
-	apiEndpointBase := os.Getenv("ENDPOINT_BASE")
-	if apiEndpointBase == "" {
-		apiEndpointBase = linebot.APIEndpointBase
-	}
-	bot, err := linebot.New(
-		channelSecret,
-		channelToken,
-		linebot.WithEndpointBase(apiEndpointBase), // Usually you omit this.
-	)
-	if err != nil {
-		return nil, err
-	}
-	downloadDir := filepath.Join(filepath.Dir(os.Args[0]), "line-bot")
-	_, err = os.Stat(downloadDir)
-	if err != nil {
-		if err := os.Mkdir(downloadDir, 0777); err != nil {
-			return nil, err
-		}
-	}
-	return &KitchenSink{
-		bot:         bot,
-		appBaseURL:  appBaseURL,
-		downloadDir: downloadDir,
-	}, nil
-}
-
-// Callback function for http server
-func (app *KitchenSink) Callback(w http.ResponseWriter, r *http.Request) {
-	events, err := app.bot.ParseRequest(r)
 	if err != nil {
 		if err == linebot.ErrInvalidSignature {
 			w.WriteHeader(400)
@@ -84,51 +32,19 @@ func (app *KitchenSink) Callback(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+
 	for _, event := range events {
-		log.Printf("Got event %v", event)
-		switch event.Type {
-		case linebot.EventTypeMessage:
+		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
-			
-			case *linebot.ImageMessage:
-				if err := app.handleImage(message, event.ReplyToken); err != nil {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
 					log.Print(err)
 				}
-			
-			default:
-				log.Printf("Unknown message: %v", message)
 			}
-		case linebot.EventTypeFollow:
-			if err := app.replyText(event.ReplyToken, "Got followed event"); err != nil {
-				log.Print(err)
-			}
-		case linebot.EventTypeUnfollow:
-			log.Printf("Unfollowed this bot: %v", event)
-		case linebot.EventTypeJoin:
-			if err := app.replyText(event.ReplyToken, "Joined "+string(event.Source.Type)); err != nil {
-				log.Print(err)
-			}
-		case linebot.EventTypeLeave:
-			log.Printf("Left: %v", event)
-		case linebot.EventTypePostback:
-			data := event.Postback.Data
-			if data == "DATE" || data == "TIME" || data == "DATETIME" {
-				data += fmt.Sprintf("(%v)", *event.Postback.Params)
-			}
-			if err := app.replyText(event.ReplyToken, "Got postback: "+data); err != nil {
-				log.Print(err)
-			}
-		case linebot.EventTypeBeacon:
-			if err := app.replyText(event.ReplyToken, "Got beacon: "+event.Beacon.Hwid); err != nil {
-				log.Print(err)
-			}
-		default:
-			log.Printf("Unknown event: %v", event)
 		}
 	}
 }
-
-func (app *KitchenSink) handleText(message *linebot.TextMessage, replyToken string, source *linebot.EventSource) error {
+func handleText(message *linebot.TextMessage, replyToken string, source *linebot.EventSource) error {
 	switch message.Text {
 	
 	case "buttons":
@@ -137,7 +53,7 @@ func (app *KitchenSink) handleText(message *linebot.TextMessage, replyToken stri
 			imageURL, "My button sample", "Hello, my button",
 			linebot.NewURITemplateAction("Go to line.me", "https://line.me"),
 			linebot.NewPostbackTemplateAction("Say hello1", "hello ", ""),
-			linebot.NewPostbackTemplateAction("hello2", "hello ", "hello "),
+			linebot.NewPostbackTemplateAction(" hello2", "hello ", "hello "),
 			linebot.NewMessageTemplateAction("Say message", "Rice="),
 		)
 		if _, err := app.bot.ReplyMessage(
@@ -157,67 +73,4 @@ func (app *KitchenSink) handleText(message *linebot.TextMessage, replyToken stri
 		}
 	}
 	return nil
-}
-
-func (app *KitchenSink) handleImage(message *linebot.ImageMessage, replyToken string) error {
-	return app.handleHeavyContent(message.ID, func(originalContent *os.File) error {
-		// You need to install ImageMagick.
-		// And you should consider about security and scalability.
-		previewImagePath := originalContent.Name() + "-preview"
-		_, err := exec.Command("convert", "-resize", "240x", "jpeg:"+originalContent.Name(), "jpeg:"+previewImagePath).Output()
-		if err != nil {
-			return err
-		}
-
-		originalContentURL := app.appBaseURL + "/downloaded/" + filepath.Base(originalContent.Name())
-		previewImageURL := app.appBaseURL + "/downloaded/" + filepath.Base(previewImagePath)
-		if _, err := app.bot.ReplyMessage(
-			replyToken,
-			linebot.NewImageMessage(originalContentURL, previewImageURL),
-		).Do(); err != nil {
-			return err
-		}
-		return nil
-	})
-}
-
-
-
-func (app *KitchenSink) replyText(replyToken, text string) error {
-	if _, err := app.bot.ReplyMessage(
-		replyToken,
-		linebot.NewTextMessage(text),
-	).Do(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (app *KitchenSink) handleHeavyContent(messageID string, callback func(*os.File) error) error {
-	content, err := app.bot.GetMessageContent(messageID).Do()
-	if err != nil {
-		return err
-	}
-	defer content.Content.Close()
-	log.Printf("Got file: %s", content.ContentType)
-	originalConent, err := app.saveContent(content.Content)
-	if err != nil {
-		return err
-	}
-	return callback(originalConent)
-}
-
-func (app *KitchenSink) saveContent(content io.ReadCloser) (*os.File, error) {
-	file, err := ioutil.TempFile(app.downloadDir, "")
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, content)
-	if err != nil {
-		return nil, err
-	}
-	log.Printf("Saved %s", file.Name())
-	return file, nil
 }
