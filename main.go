2e11c7a1815f7a38c1c6ad18c6ac6e470391ff3c
meng-tint-chien <31497704+meng-tint-chien@users.noreply.github.com>
2017-10-17 21:47:05 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	
+	"os/exec"
 	"path/filepath"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-var bot *linebot.Client
-
 func main() {
-	
 	app, err := NewKitchenSink(
 		os.Getenv("CHANNEL_SECRET"),
 		os.Getenv("CHANNEL_TOKEN"),
@@ -38,11 +37,15 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+// KitchenSink app
 type KitchenSink struct {
 	bot         *linebot.Client
 	appBaseURL  string
 	downloadDir string
 }
+
+// NewKitchenSink function
 func NewKitchenSink(channelSecret, channelToken, appBaseURL string) (*KitchenSink, error) {
 	apiEndpointBase := os.Getenv("ENDPOINT_BASE")
 	if apiEndpointBase == "" {
@@ -69,6 +72,80 @@ func NewKitchenSink(channelSecret, channelToken, appBaseURL string) (*KitchenSin
 		downloadDir: downloadDir,
 	}, nil
 }
+
+// Callback function for http server
+func (app *KitchenSink) Callback(w http.ResponseWriter, r *http.Request) {
+	events, err := app.bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+		return
+	}
+	for _, event := range events {
+		log.Printf("Got event %v", event)
+		switch event.Type {
+		case linebot.EventTypeMessage:
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if err := app.handleText(message, event.ReplyToken, event.Source); err != nil {
+					log.Print(err)
+				}
+			case *linebot.ImageMessage:
+				if err := app.handleImage(message, event.ReplyToken); err != nil {
+					log.Print(err)
+				}
+			case *linebot.VideoMessage:
+				if err := app.handleVideo(message, event.ReplyToken); err != nil {
+					log.Print(err)
+				}
+			case *linebot.AudioMessage:
+				if err := app.handleAudio(message, event.ReplyToken); err != nil {
+					log.Print(err)
+				}
+			case *linebot.LocationMessage:
+				if err := app.handleLocation(message, event.ReplyToken); err != nil {
+					log.Print(err)
+				}
+			case *linebot.StickerMessage:
+				if err := app.handleSticker(message, event.ReplyToken); err != nil {
+					log.Print(err)
+				}
+			default:
+				log.Printf("Unknown message: %v", message)
+			}
+		case linebot.EventTypeFollow:
+			if err := app.replyText(event.ReplyToken, "Got followed event"); err != nil {
+				log.Print(err)
+			}
+		case linebot.EventTypeUnfollow:
+			log.Printf("Unfollowed this bot: %v", event)
+		case linebot.EventTypeJoin:
+			if err := app.replyText(event.ReplyToken, "Joined "+string(event.Source.Type)); err != nil {
+				log.Print(err)
+			}
+		case linebot.EventTypeLeave:
+			log.Printf("Left: %v", event)
+		case linebot.EventTypePostback:
+			data := event.Postback.Data
+			if data == "DATE" || data == "TIME" || data == "DATETIME" {
+				data += fmt.Sprintf("(%v)", *event.Postback.Params)
+			}
+			if err := app.replyText(event.ReplyToken, "Got postback: "+data); err != nil {
+				log.Print(err)
+			}
+		case linebot.EventTypeBeacon:
+			if err := app.replyText(event.ReplyToken, "Got beacon: "+event.Beacon.Hwid); err != nil {
+				log.Print(err)
+			}
+		default:
+			log.Printf("Unknown event: %v", event)
+		}
+	}
+}
+
 func (app *KitchenSink) handleText(message *linebot.TextMessage, replyToken string, source *linebot.EventSource) error {
 	switch message.Text {
 	
@@ -87,7 +164,7 @@ func (app *KitchenSink) handleText(message *linebot.TextMessage, replyToken stri
 		).Do(); err != nil {
 			return err
 		}
-
+	
 	default:
 		log.Printf("Echo message to %s: %s", replyToken, message.Text)
 		if _, err := app.bot.ReplyMessage(
@@ -97,6 +174,68 @@ func (app *KitchenSink) handleText(message *linebot.TextMessage, replyToken stri
 			return err
 		}
 	}
-return nil
+	return nil
 }
 
+func (app *KitchenSink) handleImage(message *linebot.ImageMessage, replyToken string) error {
+	return app.handleHeavyContent(message.ID, func(originalContent *os.File) error {
+		// You need to install ImageMagick.
+		// And you should consider about security and scalability.
+		previewImagePath := originalContent.Name() + "-preview"
+		_, err := exec.Command("convert", "-resize", "240x", "jpeg:"+originalContent.Name(), "jpeg:"+previewImagePath).Output()
+		if err != nil {
+			return err
+		}
+
+		originalContentURL := app.appBaseURL + "/downloaded/" + filepath.Base(originalContent.Name())
+		previewImageURL := app.appBaseURL + "/downloaded/" + filepath.Base(previewImagePath)
+		if _, err := app.bot.ReplyMessage(
+			replyToken,
+			linebot.NewImageMessage(originalContentURL, previewImageURL),
+		).Do(); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+
+
+func (app *KitchenSink) replyText(replyToken, text string) error {
+	if _, err := app.bot.ReplyMessage(
+		replyToken,
+		linebot.NewTextMessage(text),
+	).Do(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (app *KitchenSink) handleHeavyContent(messageID string, callback func(*os.File) error) error {
+	content, err := app.bot.GetMessageContent(messageID).Do()
+	if err != nil {
+		return err
+	}
+	defer content.Content.Close()
+	log.Printf("Got file: %s", content.ContentType)
+	originalConent, err := app.saveContent(content.Content)
+	if err != nil {
+		return err
+	}
+	return callback(originalConent)
+}
+
+func (app *KitchenSink) saveContent(content io.ReadCloser) (*os.File, error) {
+	file, err := ioutil.TempFile(app.downloadDir, "")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, content)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("Saved %s", file.Name())
+	return file, nil
+}
